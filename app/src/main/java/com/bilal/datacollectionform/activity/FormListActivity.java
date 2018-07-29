3fb8b202d03d91875cd4c8d988d4ad821499d94c
Mohd Bilal <mohd.bilal1996@gmail.com>
2018-07-29 16:08:28 +0530

    UI

---
+++ app/src/main/java/com/bilal/datacollectionform/activity/FormListActivity.java
@@ -0,0 +1,73 @@
+package com.bilal.datacollectionform.activity;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.bilal.datacollectionform.R;
+
+public class FormListActivity extends AppCompatActivity {
+
+    private Context context;
+
+
+    private RecyclerView mRecyclerView;
+    private MyRecylerViewAdapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_from_list);
+
+        context = this;
+
+        mRecyclerView = findViewById(R.id.recyclerview);
+        mLayoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+        mAdapter = new MyRecylerViewAdapter();
+        mRecyclerView.setAdapter(mAdapter);
+    }
+
+    private class MyRecylerViewAdapter extends RecyclerView.Adapter<MyRecylerViewAdapter.ViewHolder> {
+
+        private String[] mDataset = {"Contact Form", "Survey Form"};
+
+        @Override
+        public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            LayoutInflater layoutInflater = LayoutInflater.from(context);
+            View v = layoutInflater.inflate(R.layout.list_item_form, parent, false);
+            return new ViewHolder(v);
+        }
+
+        @Override
+        public void onBindViewHolder(ViewHolder holder, int position) {
+            holder.textView.setText(mDataset[position]);
+
+        }
+
+        @Override
+        public int getItemCount() {
+            return mDataset.length;
+        }
+
+        public class ViewHolder extends RecyclerView.ViewHolder {
+
+            public TextView textView;
+            public LinearLayout linearLayout;
+
+            public ViewHolder(View v) {
+                super(v);
+                textView = v.findViewById(R.id.textview);
+                linearLayout = v.findViewById(R.id.linear_layout);
+            }
+        }
+    }
+}
